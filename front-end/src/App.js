3992ec6c2c7912c5652ab6881eb3da5333ae1b46
NikSoftelevation <122023966+NikSoftelevation@users.noreply.github.com>
2023-03-20 11:12:57 +0530

    On App.js a badge is coming that it is deleted:resolved by creating new App.js and deleted existing one

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,3 +1,4 @@
+
 import "./App.css";
 import { useEffect } from "react";
 import { useLocalState } from "./util/useLocalStorage";
@@ -5,38 +6,43 @@ import { Route, Routes } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import HomePage from "./Homepage";
 
+
 function App() {
-  const [jwt, setJwt] = useLocalState("", "jwt");
-
-  useEffect(() => {
-    if (!jwt) console.log("Hello");
-
-    const reqBody = {
-      username: "NikhilAdmin",
-      password: "Nikhil@07@18",
-    };
-
-    fetch("api/auth/login", {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      method: "post",
-      body: JSON.stringify(reqBody),
-    })
-      .then((response) => Promise.all([response.json(), response.headers]))
-      .then(([body, headers]) => {
-        setJwt(headers.get("authorization"));
-      });
-  }, []);
-  useEffect(() => {
-    console.log(`JWT is : ${jwt}`);
-  }, [jwt]);
-
-  return (
-    <Routes>
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/" element={<HomePage />} />
-    </Routes>
-  );
-}
-export default App;
+    const [jwt, setJwt] = useLocalState("", "jwt");
+  
+    useEffect(() => {
+      if (!jwt) console.log("Hello");
+  
+      const reqBody = {
+        username: "NikhilAdmin",
+        password: "Nikhil@07@18",
+      };
+  
+      fetch("api/auth/login", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "post",
+        body: JSON.stringify(reqBody),
+      })
+        .then((response) => Promise.all([response.json(), response.headers]))
+        .then(([body, headers]) => {
+          setJwt(headers.get("authorization"));
+        });
+    }, []);
+    useEffect(() => {
+      console.log(`JWT is : ${jwt}`);
+    }, [jwt]);
+  
+    return (
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={<HomePage />}
+            />
+      </Routes>
+    );
+  }
+  export default App;
+  
