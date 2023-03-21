cc2dc60583c6fa6855bb7912984077debe857a28
NikSoftelevation <122023966+NikSoftelevation@users.noreply.github.com>
2023-03-21 12:58:42 +0530

    Unused and unwanted files are removed in backend

--- src/main/java/com/coderscampus/AssisgnmentSubmissionApp/service/CreateUserService.java
+++
@@ -1,7 +0,0 @@
-package com.coderscampus.AssisgnmentSubmissionApp.service;
-
-import com.coderscampus.AssisgnmentSubmissionApp.model.User;
-
-public interface CreateUserService {
-    public User createUser(User user);
-}
