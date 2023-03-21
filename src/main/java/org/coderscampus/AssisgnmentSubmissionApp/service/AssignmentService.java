f5fa176ce50cc006bfe6191cc69ac9cfcd3aa887
NikSoftelevation <122023966+NikSoftelevation@users.noreply.github.com>
2023-03-21 19:34:53 +0530

    Reduced unwanted spaces in AssignmentService in backend

--- src/main/java/org/coderscampus/AssisgnmentSubmissionApp/service/AssignmentService.java
+++ src/main/java/org/coderscampus/AssisgnmentSubmissionApp/service/AssignmentService.java
@@ -5,30 +5,24 @@ import org.coderscampus.AssisgnmentSubmissionApp.model.User;
 import org.coderscampus.AssisgnmentSubmissionApp.repository.AssignmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.Optional;
 import java.util.Set;
-
 @Service
 public class AssignmentService {
     @Autowired
     private AssignmentRepository assignmentRepository;
-
     public Assignment save(User user) {
         Assignment assignment = new Assignment();
         assignment.setStatus("Needs to be submitted");
         assignment.setUser(user);
         return assignmentRepository.save(assignment);
     }
-
     public Set<Assignment> findByUser(User user) {
         return assignmentRepository.findByUser(user);
     }
-
     public Optional<Assignment> findAssignmentByAssignmentId(int id) {
         return assignmentRepository.findById(id);
     }
-
     public Assignment updateAssignment(Assignment assignment) {
         return assignmentRepository.save(assignment);
     }
