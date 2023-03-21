55f71bd13531e678061c0cd3626906203f0d16ec
NikSoftelevation <122023966+NikSoftelevation@users.noreply.github.com>
2023-03-21 11:14:46 +0530

    React privateroute created and implemented initially and redirection is also done if it is not logged in

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,48 +1,51 @@
-
 import "./App.css";
 import { useEffect } from "react";
 import { useLocalState } from "./util/useLocalStorage";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import HomePage from "./Homepage";
-
+import Login from "./Login";
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
-    const [jwt, setJwt] = useLocalState("", "jwt");
-  
-    useEffect(() => {
-      if (!jwt) console.log("Hello");
-  
-      const reqBody = {
-        username: "NikhilAdmin",
-        password: "Nikhil@07@18",
-      };
-  
-      fetch("api/auth/login", {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "post",
-        body: JSON.stringify(reqBody),
-      })
-        .then((response) => Promise.all([response.json(), response.headers]))
-        .then(([body, headers]) => {
-          setJwt(headers.get("authorization"));
-        });
-    }, []);
-    useEffect(() => {
-      console.log(`JWT is : ${jwt}`);
-    }, [jwt]);
-  
-    return (
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route
-          path="/"
-          element={<HomePage />}
-            />
-      </Routes>
-    );
-  }
-  export default App;
-  
+  const [jwt, setJwt] = useLocalState("", "jwt");
+
+  useEffect(() => {
+    if (!jwt) console.log("Hello");
+
+    const reqBody = {
+      username: "NikhilAdmin",
+      password: "Nikhil@07@18",
+    };
+
+    fetch("api/auth/login", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "post",
+      body: JSON.stringify(reqBody),
+    })
+      .then((response) => Promise.all([response.json(), response.headers]))
+      .then(([body, headers]) => {
+        setJwt(headers.get("authorization"));
+      });
+  }, []);
+  useEffect(() => {
+    console.log(`JWT is : ${jwt}`);
+  }, [jwt]);
+
+  return (
+    <Routes>
+      <Route path="/dashboard" element={
+      <PrivateRoute>
+      <Dashboard />
+     </PrivateRoute>
+
+      
+      } />
+      <Route path="login" element={<Login />} />
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+  );
+}
+export default App;
