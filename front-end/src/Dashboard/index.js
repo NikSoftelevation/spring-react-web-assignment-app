eb8599dd098d475f0af3e4378eef91765c22eac8
NikSoftelevation <122023966+NikSoftelevation@users.noreply.github.com>
2023-03-23 11:32:27 +0530

    Working with Bootstrap Cards and CSS Grid

--- front-end/src/Dashboard/index.js
+++ front-end/src/Dashboard/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Button, Card } from "react-bootstrap";
+import { Link, Navigate } from "react-router-dom";
 import ajax from "../Services/fetchService";
 import { useLocalState } from "../util/useLocalStorage";
 
@@ -10,7 +11,7 @@ const Dashboard = () => {
     ajax("api/assignments", "GET", jwt).then((assignmentsData) => {
       setAssignments(assignmentsData);
     });
-  }, []);
+  }, [jwt]);
 
   function createAssignment() {
     ajax("api/assignments", "POST", jwt).then((assignment) => {
@@ -18,20 +19,50 @@ const Dashboard = () => {
     });
   }
   return (
-    <div style={{ margin: "2cm" }}>
+    <div style={{ margin: "2em" }}>
+      <div className="mb-5">
+      <Button size="lg" onClick={() => createAssignment()}>Submit New Assignment</Button>
+
+
+      </div>
       {assignments ? (
-        assignments.map((assignment) => (
-          <div key={assignment.id}>
-            <Link to={`/assignments/${assignment.id}`}>
-              Assignment ID:{assignment.id}
-            </Link>
-          </div>
-        ))
+        <div
+          className="d-grid gap-5"
+          style={{ gridTemplateColumns: "repeat(auto-fit,18rem)" }}
+        >
+          {assignments.map((assignment) => (
+            <Card
+              key={assignment.id}
+              style={{ width: "18rem", height: "18rem" }}
+            >
+              <Card.Body className="d-flex flex-column justify-content-around">
+                <Card.Title>Assignment #{assignment.id}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  {assignment.status}
+                </Card.Subtitle>
+                <Card.Text style={{ marginTop: "1em" }}>
+                  <p>
+                    <b>GitHub URL</b>: {assignment.githubUrl}
+                  </p>
+                  <p>
+                    <b>Branch</b>: {assignment.branch}
+                  </p>
+                </Card.Text>
+                <Button variant="secondary"
+                  onClick={() => {
+                    window.location.href = `/assignments/${assignment.id}`;
+                  }}
+                >
+                  Edit
+                </Button>
+              </Card.Body>
+            </Card>
+          ))}
+        </div>
       ) : (
         <></>
       )}
 
-      <button onClick={() => createAssignment()}>Submit New Assignment</button>
     </div>
   );
 };
