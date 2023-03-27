101cb493ff5c7cc9e24111f60b8e34247712bae3
NikSoftelevation <122023966+NikSoftelevation@users.noreply.github.com>
2023-03-27 14:17:16 +0530

    Filtering Arrays with Fat Arrow Syntax

--- front-end/src/CodeReviewerDashboard/index.js
+++ front-end/src/CodeReviewerDashboard/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { Badge, Button, Card, Col, Container, Row } from "react-bootstrap";
 import { Link, Navigate } from "react-router-dom";
 import ajax from "../Services/fetchService";
 import { useLocalState } from "../util/useLocalStorage";
@@ -56,57 +56,149 @@ const CodeReviewerDashboard = () => {
           <div className="h1">Code Reviewer Dashboard</div>
         </Col>
       </Row>
-      {/*<div className="assignment-wrapper in-review"></div>*/}
+      <div className="assignment-wrapper in-review">
+        <div className="assignment-wrapper-title h3 px-2 ">In Review</div>
+        {assignments &&
+        assignments.filter((assignment) => assignment.status === "In Review")
+          .length > 0 ? (
+          <div
+            className="d-grip gap-5"
+            style={{ gridTemplateColumns: "repeat(auto-fit,18rem)" }}
+          >
+            {assignments
+              .filter((assignment) => assignment.status === "In Review")
+              .map((assignment) => (
+                <Card
+                  key={assignment.id}
+                  style={{ width: "18rem", height: "18rem" }}
+                >
+                  <Card.Body className="d-flex flex-column justify-content-around">
+                    <Card.Title>Assignment #{assignment.number}</Card.Title>
+                    <div className="d-flex align-items-start">
+                      <Badge
+                        pill
+                        bg="info"
+                        style={{
+                          fontSize: "1em",
+                        }}
+                      >
+                        {assignment.status}
+                      </Badge>
+                    </div>
+
+                    <Card.Text style={{ marginTop: "1em" }}>
+                      <p>
+                        <b>GitHub Url </b> : {assignment.githubUrl}
+                      </p>
+                      <p>
+                        <b>Branch </b> : {assignment.branch}
+                      </p>
+                    </Card.Text>
+
+                    <Button
+                      variant="secondary"
+                      onClick={() => {
+                        claimAssignment(assignment);
+                      }}
+                    >
+                      Claim
+                    </Button>
+                  </Card.Body>
+                </Card>
+              ))}
+          </div>
+        ) : (
+          <div>No assignments found!</div>
+        )}
+      </div>
       <div className="assignment-wrapper submitted">
-        <div
-          className="h3"
-          style={{
-            width: "min-content",
-            marginTop: "-2em",
-            marginBottom: "1em",
-            backgroundColor: "white",
-            whiteSpace: "nowrap",
-          }}
-        >
-          Awaiting Review
-        </div>
-        {assignments ? (
+        <div className="assignment-wrapper-title h3 px-2 ">Awaiting Review</div>
+        {assignments &&
+        assignments.filter((assignment) => assignment.status === "Submitted")
+          .length > 0 ? (
           <div
             className="d-grid gap-5"
             style={{ gridTemplateColumns: "repeat(auto-fit,18rem)" }}
           >
-            {assignments.map((assignment) => (
-              <Card
-                key={assignment.id}
-                style={{ width: "18rem", height: "18rem" }}
-              >
-                <Card.Body className="d-flex flex-column justify-content-around">
-                  <Card.Title>Assignment #{assignment.id}</Card.Title>
-                  <Card.Subtitle className="mb-2 text-muted">
-                    {assignment.status}
-                  </Card.Subtitle>
-                  <Card.Text style={{ marginTop: "1em" }}>
-                    <p>
-                      <b>GitHub URL</b>: {assignment.githubUrl}
-                    </p>
-                    <p>
-                      <b>Branch</b>: {assignment.branch}
-                    </p>
-                  </Card.Text>
-                  <Button
-                    variant="secondary"
-                    onClick={() => {
-                      claimAssignment(assignment);
-                    }}
-                  >
-                    Claim
-                  </Button>
-                </Card.Body>
-              </Card>
-            ))}
+            {assignments
+              .filter((assignment) => assignment.status === "Submitted")
+              .map((assignment) => (
+                <Card
+                  key={assignment.id}
+                  style={{ width: "18rem", height: "18rem" }}
+                >
+                  <Card.Body className="d-flex flex-column justify-content-around">
+                    <Card.Title>Assignment #{assignment.id}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">
+                      {assignment.status}
+                    </Card.Subtitle>
+                    <Card.Text style={{ marginTop: "1em" }}>
+                      <p>
+                        <b>GitHub URL</b>: {assignment.githubUrl}
+                      </p>
+                      <p>
+                        <b>Branch</b>: {assignment.branch}
+                      </p>
+                    </Card.Text>
+                    <Button
+                      variant="secondary"
+                      onClick={() => {
+                        claimAssignment(assignment);
+                      }}
+                    >
+                      Claim
+                    </Button>
+                  </Card.Body>
+                </Card>
+              ))}
           </div>
         ) : (
-          <></>
+          <div>No assignments found!</div>
+        )}
+      </div>
+      <div className="assignment-wrapper needs-update">
+        <div className="assignment-wrapper-title h3 px-2 ">Needs Update</div>
+        {assignments &&
+        assignments.filter((assignment) => assignment.status === "Needs update")
+          .length > 0 ? (
+          <div
+            className="d-grid gap-5"
+            style={{ gridTemplateColumns: "repeat(auto-fit,18rem)" }}
+          >
+            {assignments
+              .filter((assignment) => assignment.status === "Needs Update")
+              .map((assignment) => (
+                <Card
+                  key={assignment.id}
+                  style={{ width: "18rem", height: "18rem" }}
+                >
+                  <Card.Body className="d-flex flex-column justify-content-around">
+                    <Card.Title>Assignment #{assignment.id}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">
+                      {assignment.status}
+                    </Card.Subtitle>
+                    <Card.Text style={{ marginTop: "1em" }}>
+                      <p>
+                        <b>GitHub URL</b>: {assignment.githubUrl}
+                      </p>
+                      <p>
+                        <b>Branch</b>: {assignment.branch}
+                      </p>
+                    </Card.Text>
+                    <Button
+                      variant="secondary"
+                      onClick={() => {
+                        claimAssignment(assignment);
+                      }}
+                    >
+                      Claim
+                    </Button>
+                  </Card.Body>
+                </Card>
+              ))}
+          </div>
+        ) : (
+          <div>No assignments found!</div>
         )}
       </div>
     </Container>
